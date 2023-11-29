c91a5ee8450aa5495843bac03a93bc4f15b779c2
pronoyNath <pronoynath890@gmail.com>
2023-11-30 03:44:04 +0600

    blog filter addded

--- src/Pages/Dashboard/ContentManagement.jsx
+++ src/Pages/Dashboard/ContentManagement.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
 import BlogCard from '../../components/BlogCard/BlogCard';
 import axiosSecure from '../../hooks/useAxiosSecure';
+import { useState } from 'react';
 
 const ContentManagement = () => {
     const {user} = useAuth();
+    // const [selectedValue,setSelectedValue] = useState('all');
 
     // tanstack query for updated data get 
     const { data: blogs = [], isLoading,refetch } = useQuery({
@@ -16,6 +18,23 @@ const ContentManagement = () => {
         }
     })
 
+    const [filteredBlogs, setFilteredBlogs] = useState(blogs);
+
+    const handleFilter = async (e) => {
+        const selectedValue = e.target.value;
+        // console.log(selectedValue);
+
+        // Filter blogs based on the selected donation status
+        const filtered = blogs.filter(blg => {
+            if (selectedValue === 'all') {
+                return true;
+            } else {
+                return blg.blogStatus === selectedValue;
+            }
+        });
+
+        setFilteredBlogs(filtered);
+    };
 
     return (
         <div className=''>
@@ -26,11 +45,21 @@ const ContentManagement = () => {
             <h3 className='text-center text-3xl font-bold my-5 uppercase'>Your All Blogs</h3>
 
           </div>
+          <div className="my-5 text-black mr-8 ml-5 lg:ml-0 flex gap-3 items-center ">
+                <div className="text-lg mt-2 lg:mt-0 text-red-500 font-bold ">Filter By User Staus:</div>
+                
+                <select onChange={handleFilter} className="rounded p-1 ml-5 lg:ml-0 mt-5 lg:mt-0 bg-red-500 text-white lg:text-lg " name="filter" id="filter"
+                >
+                    <option value="all">All</option>
+                    <option value="publish">Published</option>
+                    <option value="draft">Draft</option>
+                </select>
+            </div>
 
             <div className='grid grid-cols-1 gap-10'>
                 
                 {
-                   blogs.length>0 ? blogs.map(blog=><BlogCard refetch={refetch}
+                   filteredBlogs.length > 0 ? filteredBlogs.map(blog=><BlogCard refetch={refetch}
                         key={blog._id}
                         blog={blog}
                     ></BlogCard>) 
